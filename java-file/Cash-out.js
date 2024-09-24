911204e55f6c3229961021a743c6ca2f76ac0396
mutasim99 <virginmr882@gmail.com>
2024-09-24 20:02:16 +0600

    add cash out history

--- java-file/Cash-out.js
+++ java-file/Cash-out.js
@@ -10,6 +10,13 @@ document.getElementById('cash-out-btn')
             const currentBalanceMoney = parseFloat(currentBalance);
             const totalBalance = currentBalanceMoney - cashOutMoney;
             document.getElementById('balance').innerText = totalBalance;
+            const div = document.createElement('div');
+            div.className = "bg-white rounded-md p-3 border-l-2 border-indigo-500 mt-2"
+            div.innerHTML = `
+                <p class="text-gray-500 text-xs">${new Date().toLocaleString()}</p>
+                <p class="text-gray-500 text-xs">Cash out money:${cashOutMoney} tk. New balance:${totalBalance}</P>
+            `
+            document.getElementById('history-container').appendChild(div);
         }
         else{
             alert('pin you provide is  not correct');
--- java-file/home.js
+++ java-file/home.js
@@ -11,7 +11,7 @@ document.getElementById('add-money-btn')
             const newBalance = amountMoney + balanceMoney;
             document.getElementById('balance').innerText = newBalance
             const div = document.createElement('div');
-            div.className = "bg-white rounded-md p-3 border-l-2 border-indigo-500"
+            div.className = "bg-white rounded-md p-3 border-l-2 border-indigo-500 mt-2"
             div.innerHTML = `
                 <p class="text-gray-500 text-xs">${new Date().toLocaleString()}</p>
                 <p class="text-gray-500 text-xs">Add money:${amountMoney} tk. New balance:${newBalance}</P>
